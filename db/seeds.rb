7a651732bad5038624d4619593e9f416ac9ebb1a
Andrew Yasso <andrew.yasso@gmail.com>
2018-06-13 15:20:45 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+
+1.times do
+menu = Menu.create(name: 'Breakfast')
+    15.times do
+    menu.items.create(
+      name: Faker::Food.dish,
+      price: Faker::Number.between(1, 30),
+    )
+    end
+end
+
+
+1.times do
+  menu = Menu.create(name: 'Lunch')
+      15.times do
+      menu.items.create(
+        name: Faker::Food.dish,
+        price: Faker::Number.between(1, 30),
+      )
+      end
+  end
+
+1.times do
+  menu = Menu.create(name: 'Breakfast')
+      15.times do
+      menu.items.create(
+        name: Faker::Food.dish,
+        price: Faker::Number.between(1, 30),
+      )
+      end
+  end
